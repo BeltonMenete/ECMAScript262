46c232049441332fbff3c1a2603b552acd859728
DevAlberto <beltonmenete@hotmail.com>
2024-10-25 16:02:40 +0200

    Add new Files

---
+++ starter/js.js
@@ -0,0 +1,13 @@
+// Calculate BMI
+
+// Mark's height
+
+const bmiMark = 78 / 1.69 ** 2;
+const bmiJohn = 92 / 1.95 ** 2;
+const isMarkHigherBMI = bmiMark > bmiJohn;
+
+if (isMarkHigherBMI) {
+  console.log(`Mark's ${bmiMark.toFixed(1)} BMI is higher than John's ${bmiJohn.toFixed(1)}`);
+} else {
+  console.log(`John's ${bmiJohn.toFixed(2)} BMI is higher than Mark's ${bmiMark.toFixed()}`);
+}
