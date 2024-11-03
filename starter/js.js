1438350871f7d06c6f92c1e26a669e400ec03a75
DevAlberto <beltonmenete@hotmail.com>
2024-11-03 12:50:20 +0200

    Update stuff

--- starter/js.js
+++ starter/js.js
@@ -1,13 +1,21 @@
-// Calculate BMI
+// coding challenge 1,2
 
-// Mark's height
+// // Mark's height
 
-const bmiMark = 78 / 1.69 ** 2;
-const bmiJohn = 92 / 1.95 ** 2;
-const isMarkHigherBMI = bmiMark > bmiJohn;
+// const bmiMark = 78 / 1.69 ** 2;
+// const bmiJohn = 92 / 1.95 ** 2;
+// const isMarkHigherBMI = bmiMark > bmiJohn;
 
-if (isMarkHigherBMI) {
-  console.log(`Mark's ${bmiMark.toFixed(1)} BMI is higher than John's ${bmiJohn.toFixed(1)}`);
-} else {
-  console.log(`John's ${bmiJohn.toFixed(2)} BMI is higher than Mark's ${bmiMark.toFixed()}`);
+// if (isMarkHigherBMI) {
+//   console.log(`Mark's ${bmiMark.toFixed(1)} BMI is higher than John's ${bmiJohn.toFixed(1)}`);
+// } else {
+//   console.log(`John's ${bmiJohn.toFixed(2)} BMI is higher than Mark's ${bmiMark.toFixed()}`);
+// }
+
+console.log(`This is an amazing \n\
+   increase \n\
+   strengthen \n\ : skill `);
+
+if (true) {
+  console.log(true);
 }
