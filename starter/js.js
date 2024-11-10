5739f55f47b99e5a6dc199155a09914aefab26c6
DevAlberto <beltonmenete@hotmail.com>
2024-11-10 16:44:53 +0200

    Ok

--- starter/js.js
+++ starter/js.js
@@ -1,13 +1,7 @@
-// Calculate BMI
+"use strict";
 
-// Mark's height
+let hasDriversLicense = false;
+const passTest = true;
 
-const bmiMark = 78 / 1.69 ** 2;
-const bmiJohn = 92 / 1.95 ** 2;
-const isMarkHigherBMI = bmiMark > bmiJohn;
-
-if (isMarkHigherBMI) {
-  console.log(`Mark's ${bmiMark.toFixed(1)} BMI is higher than John's ${bmiJohn.toFixed(1)}`);
-} else {
-  console.log(`John's ${bmiJohn.toFixed(2)} BMI is higher than Mark's ${bmiMark.toFixed()}`);
-}
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log("He/she can drive! haa");
---
+++ starter/js2.js
@@ -0,0 +1,23 @@
+'use strict';
+/* 
+//average function
+const calAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+
+//both averages
+const averageKoalas = calAverage(23, 34, 27);
+const averageDolphins = calAverage(85, 54, 41);
+
+//checking winner
+const checkWinner = (avgDolphins, avgKoalas) => {
+    if (avgDolphins >= avgKoalas * 2) console.log(`Dolphins wins ( ${avgDolphins} vs. ${avgKoalas})`);
+    else if (avgKoalas > avgDolphins * 2) console.log(`Koalas wins ( ${avgKoalas} vs. ${avgDolphins})`);
+    else console.log("No winner!");
+};
+
+checkWinner(averageDolphins, averageKoalas);
+ */
+
+// Arrays
+const allFriends = ['Michael', 'Stephen', 'Peter'];
+
+console.log(allFriends);
