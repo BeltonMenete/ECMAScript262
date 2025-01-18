bbf90c9c82da67343cc7ae7a7c30139f53ef816a
DevAlberto <beltonmenete@hotmail.com>
2025-01-18 16:09:43 +0200

    update 2 files and create 1 file

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const buttonCloseModal = document.querySelector('.close-modal');
+const buttonsOpenModal = document.querySelectorAll('.show-modal');
+
+function openModal() {
+  overlay.classList.remove('hidden');
+  modal.classList.remove('hidden');
+}
+
+function closeModal() {
+  overlay.classList.add('hidden');
+  modal.classList.add('hidden');
+}
+
+// Attach event listeners
+buttonsOpenModal.forEach((button) => {
+  button.addEventListener('click', openModal);
+});
+overlay.addEventListener('click', closeModal);
+buttonCloseModal.addEventListener('click', closeModal);
